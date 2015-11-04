772ede1581c3fd0ecae15cd9126f1d58bdf9657c
Liam Dickson <liamsdickson@gmail.com>
2015-11-03 21:07:47 -0500

    Ported Desktop Navbar

--- src/home/components/Navigation.jsx
+++ src/home/components/Navigation.jsx
@@ -1,34 +1,17 @@
 'use strict';
 
 var React = require('react');
-var {Navbar, Nav, NavItem, MenuItem} = require('react-bootstrap');
+var DesktopNav = require('./Nav/DesktopNav');
 var Navigation;
 
 module.exports = Navigation = React.createClass({
     render: function () {
         return (
-            <div>
-                <div className="navmenu navmenu-default navmenu-fixed-left offcanvas-sm">
-                  <a className="navmenu-brand visible-md visible-lg" href="#">On The Quad</a>
-                  <ul className="nav navmenu-nav">
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Account</a></li>
-                  </ul>
+            <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div className="container-fluid">
+                    <DesktopNav {...this.props} />
                 </div>
-
-                <nav className="navbar navbar-default navbar-fixed-top hidden-md hidden-lg">
-                    <button type="button" className="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu">
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">On The Quad</a>
-                    <button type="button" className="btn btn-default navbar-btn pull-right">
-                        <span className="glyphicon glyphicon-user"></span>
-                    </button>
-                </nav>
-            </div>
+            </nav>
         );
     }
 });
