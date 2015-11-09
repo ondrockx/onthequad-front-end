f43b57bc60bd0bc29b49a23e90575ba1ea3a810c
Liam Dickson <liam.s.dickson@gmail.com>
2015-11-09 11:47:02 -0500

    fixed npm dependencies and navbar behaviors

--- src/home/components/Nav/DesktopNavFilters.jsx
+++ src/home/components/Nav/DesktopNavFilters.jsx
@@ -8,14 +8,15 @@ module.exports = DesktopNavFilters = React.createClass({
     render: function () {
         return (
             <div className="hidden-md hidden-sm hidden-xs">
-                <div className="col-lg-offset-2 col-lg-2">
-                    <div className="navbar-brand">
-                        <p className="category-title">CATEGORY_FILLER</p>
-                    </div>
-                </div>
-                
-                <div className="col-lg-offset-2 col-lg-6">
-                    <NavFilters desktop {...this.props} />
+            	<div className="container">
+	                <div className="col-lg-4">
+	                    <div className="navbar-brand">
+	                        <p className="category-title">CATEGORY_FILLER</p>
+	                    </div>
+	                </div>
+	                <div className="col-lg-8">
+	                    <NavFilters desktop {...this.props} />
+	                </div>
                 </div>
             </div>
         );
