4a3fe588b9b2e0223c0a34df6574c7e0aad250fd
Liam Dickson <liam.s.dickson@gmail.com>
2016-04-28 20:59:44 -0400

    better upload

--- src/containers/PostingForm.js
+++ src/containers/PostingForm.js
@@ -1,9 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import _ from 'lodash';
 import $ from 'jquery';
+import Dropzone from 'react-dropzone';
 import { connect } from 'react-redux';
 import { addPosting } from '../actions';
-import { Input, ButtonInput, Nav, NavItem } from 'react-bootstrap';
+import { Col, Row, Input, ButtonInput, Nav, NavItem, Thumbnail } from 'react-bootstrap';
 import ProgressBox from '../components/ProgressBox';
 import config, { numToCategory, allTrue, allFalse, decodeText } from '../config';
 
@@ -18,7 +19,16 @@ class PostingForm extends Component {
         description: false,
         category: false
       },
-      category: item.category
+      category: item.category,
+      files: []
+    });
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDrop(files) {
+    console.log(files);
+    this.setState({
+      files: _.union(this.state.files, files)
     });
   }
 
@@ -28,10 +38,10 @@ class PostingForm extends Component {
         return;
       }
       const title = this.refs.title.getValue();
-      const cost = parseFloat(this.refs.cost.getValue());
+      const cost = parseFloat(this.refs.cost.getValue()) || 0.0;
       const description = this.refs.description.getValue();
       const category = parseInt(this.refs.category.getValue());
-      const files = this.refs.image.refs.input.files || [];
+      const files = this.state.files;
       const data = new FormData();
       for (var i = 0; i < files.length; i++) {
         data.append('images[]', files[i], files[i].name);
@@ -86,11 +96,12 @@ class PostingForm extends Component {
 
   render() {
     const { status, message } = this.props.postStatus;
-    const { invalid } = this.state;
+    const { invalid, files } = this.state;
     const item = this.props.item || {};
     if (status != 0) { // 0 is default
       return <div className="row"><ProgressBox {...this.props.postStatus}/></div>;
     }
+    var styles = {border: "1px solid black", width: 600, color: "black", padding: 20};
     var imageInput = <div className="form-image-upload">
       <Input
         bsStyle={ invalid['image'] ? "error" : null }
@@ -127,7 +138,32 @@ class PostingForm extends Component {
             placeholder="Item Name"
             defaultValue={decodeText(item.title) || null}
             ref="title"/>
-          {imageInput}
+          <div className='center-wrapper'>
+            <div>
+              <Dropzone accept="image/*" ref="dropzone" onDrop={this.onDrop} >
+                <div className="upload-text">Click or Drag Image Here</div>
+              </Dropzone>
+              <br />
+              <table className="table table-bordered table-condensed">
+                <tbody>
+                {_.map(files, (file, pos)=>{
+                  var shortName = file.name.substring(0, 25);
+                  shortName = shortName === file.name ? shortName : shortName + '...';
+                  return <tr><td>{shortName}</td>
+                    <td><button type="button" className="btn btn-danger btn-default btn-sm" onClick={(e)=>{
+                      e.preventDefault();
+                      this.setState({files: _.filter(files, (f) => {
+                        return f !== file;
+                      })});
+                    }}>
+                      <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                    </button></td>
+                  </tr>;
+                })}
+                </tbody>
+              </table>
+            </div>
+          </div>
           {costInput}
           <Input
             className="description-input"
